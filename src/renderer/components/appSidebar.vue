7a5c4179a1a4a3a463abf562680bd9546f727248
[MEADOW_DEV] <bendixon50@gmail.com>
2017-11-26 05:05:28 +0000

    :hankey: Temp fix for sidebar footer

--- src/renderer/components/appSidebar.vue
+++ src/renderer/components/appSidebar.vue
@@ -42,29 +42,31 @@
               </v-list-tile-action>
             </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-action>
-                <v-tooltip right>
-                  <v-btn flat slot="activator" :icon='smallItemBtn' @click.stop="showAccountsDialog = !showAccountsDialog">
-                    <v-icon>group</v-icon>
-                  </v-btn>
-                  <span>Accounts</span>
-                  <modalAccounts></modalAccounts>
-                 </v-tooltip>
-              </v-list-tile-action>
-            </v-list-tile>
+            <section style='position: fixed; bottom: 10px;left: -18px;'>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-tooltip right>
+                    <v-btn flat slot="activator" :icon='smallItemBtn' @click.stop="showAccountsDialog = !showAccountsDialog">
+                      <v-icon>group</v-icon>
+                    </v-btn>
+                    <span>Accounts</span>
+                    <modalAccounts></modalAccounts>
+                  </v-tooltip>
+                </v-list-tile-action>
+              </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-action>
-                <v-tooltip right>
-                  <v-btn flat slot="activator" :icon='smallItemBtn' @click.stop="showSettingsDialog = !showSettingsDialog">
-                    <v-icon>settings</v-icon>
-                  </v-btn>
-                  <span>Settings</span>
-                  <modalSettings></modalSettings>
-                 </v-tooltip>
-              </v-list-tile-action>
-            </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-tooltip right>
+                    <v-btn flat slot="activator" :icon='smallItemBtn' @click.stop="showSettingsDialog = !showSettingsDialog">
+                      <v-icon>settings</v-icon>
+                    </v-btn>
+                    <span>Settings</span>
+                    <modalSettings></modalSettings>
+                  </v-tooltip>
+                </v-list-tile-action>
+              </v-list-tile>
+            </section>
 
         </v-list>
     </v-navigation-drawer>
