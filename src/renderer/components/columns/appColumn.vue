9a6d3718f747ca151c343a6dcfefd50592e7c029
Zed Spencer-Milnes <zed@gingergeek.co.uk>
2017-12-15 22:02:33 +0000

    Rework sample tweets to randomly sample from pool of data.
    Data generated with https://www.mockaroo.com/fef6f860

--- src/renderer/components/columns/appColumn.vue
+++ src/renderer/components/columns/appColumn.vue
@@ -6,9 +6,7 @@
               <v-layout row align-center justify-center v-scroll="{target: '#scroll-area'}">
                   <v-flex xs12>
                       <appColumnTitlebar :icon='icon' :title='title' :username='username'/>
-                      <v-list v-for='i in 20' :key='i' style='padding: 0;'>
-                          <appTweet />
-                      </v-list>
+                      <appTweet :forColumn='columnId'/>
                   </v-flex>
               </v-layout>
           </v-container>
