47adcbad192f7c95fb14d85e35f3482e5d8923bf
Zed Spencer-Milnes <zed@gingergeek.co.uk>
2017-12-16 19:05:16 +0000

    :star: WIP New tweet design

--- src/renderer/components/tweets/appTweet.vue
+++ src/renderer/components/tweets/appTweet.vue
@@ -1,18 +1,24 @@
 <template>
 
         <!-- Main tweet card -->
-  <section>
-        <v-card v-for='i in tweets' :value='i.active' :key='i.title'>
+  <section style="padding-top: 10px; padding-bottom: 10px">
+        <v-card v-for='i in tweets' :value='i.active' :key='i.title'  class="column-card">
 
-          <v-card-text>
-            <img class="wrapping-avatar" v-bind:src='i.avatar' />
-            <h3><span v-html='i.displayName' />   <small><span v-html='i.username' /></small></h3>
+          <v-card-title class="tweet-title">
+            <img class="wrapping-avatar" v-bind:src='i.avatar'/>
+            <h3 class="name-and-username"><span v-html='i.displayName' /><br/><small><span v-html='i.username' /></small></h3>
+          </v-card-title>
+          <v-card-text class="tweet-text">
             <p><span  v-html='i.text' /></p>
+            <!-- TODO <app-tweet-actions /> ~ Zed 16/12/17-->
           </v-card-text>
+          <v-card-media v-if="i.attachment" :src="i.attachment.img.src" height="175px" class="tweet-media"/>
+          <!-- TODO support multiple attachments - need to update tweet model so attachment is array ~ Zed 16/12/17 -->
+          <v-divider/>
 
         </v-card>
         <!-- Tweet actions tab drop down -->
-        <appTweetActions />
+
   </section>
 </template>
 
@@ -39,20 +45,35 @@
 </script>
 
 <style>
-  .show-all {
-    -webkit-line-clamp: inherit !important;
-  }
-  .auto-height > .list__tile {
-    height: auto;
-    padding-top: 5px;
-    padding-bottom: 5px;
+
+  .column-card {
+    padding-top: 4px;
+    padding-bottom: 4px;
   }
 
   .wrapping-avatar {
-    float: left;
+    border-radius: 50%;
+    padding-right: 15px;
+    width: auto;
   }
 
-  .allow-wrapping-images {
-    flex-direction: row;
+  .tweet-title {
+    vertical-align: middle;
+    padding-top: 0;
+    padding-bottom: 5px;
+    height: 48px;
+  }
+
+  .tweet-text {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .tweet-media {
+    margin-bottom: 5px; /* So the separator is still visible */
+  }
+
+  .name-and-username {
+    -webkit-line-clamp: 1;
   }
 </style>
